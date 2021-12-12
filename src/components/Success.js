caa6d8d79b1af133559f244ee792e9144f9ae123
Isabela <isabela_cmoreira@hotmail.com>
2021-12-12 14:54:56 -0300

    INserção da logo na tela de sucesso

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,11 +1,16 @@
-import party from 'src/assets/party.png';
+import party from '../assets/party.png';
+import LogoMini from '../components/LogoMini.js'
 
 export default function Success (){
     return(
-    <div className="box">
-    <p className="font">PARABÉNS!</p>
-    <img src={party} alt= "emoji comemorando"/>
-    <p className="lettering">Você não esqueceu de nenhum flashcard!</p>
+        <>
+    <LogoMini/>
+        <div className="box">
+        <p className="font">PARABÉNS!</p>
+        <img src={party} alt= "emoji comemorando"/>
+        <p className="lettering">Você não esqueceu de nenhum flashcard!</p>
+        </div>
+        </>
     );
-    </div>
+  
 }
