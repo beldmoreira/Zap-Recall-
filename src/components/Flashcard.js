9ed5dd1002de6757530951effb3a6717233b3921
Isabela <isabela_cmoreira@hotmail.com>
2021-12-12 15:56:19 -0300

    Criação do componente Flashcard

---
+++ src/components/Flashcard.js
@@ -0,0 +1,13 @@
+import LogoMini from "./LogoMini";
+
+export default function Pagina(){
+    return(
+        <>
+        <LogoMini/>
+        <div className="card-front">
+        
+        </div>
+        </>
+    );
+  
+  }
