f1db122517ab40d92a739bab279a18c1868bbc35
Isabela <isabela_cmoreira@hotmail.com>
2021-12-12 15:30:54 -0300

    últimos ajustes de posicionamento na tela de fracasso

--- src/components/Failure.js
+++ src/components/Failure.js
@@ -8,8 +8,13 @@ export default function Failure(){
         <div className="box">
         <p className="font">Putz.. </p>
         <img src={sad} alt= "emoji chorando"/>
-        <p className = "lettering">Você esqueceu alguns flashcards..Não desanime! Na próxima você consegue!</p>
         </div> 
+        <div className="phrase">
+        <p className = "lettering">Você esqueceu alguns flashcards..</p>
+        </div>
+        <div className= "phrase">
+        <p className = "lettering">Não desanime! Na próxima você consegue!</p>
+        </div>
         </>
         );
     }
