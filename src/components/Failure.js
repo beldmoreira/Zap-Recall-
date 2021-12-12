cd7400e220f90b15ed26131383fed064c7724d3d
Isabela <isabela_cmoreira@hotmail.com>
2021-12-12 14:55:51 -0300

    Inserção de logo mini na tela de fracasso

--- src/components/Failure.js
+++ src/components/Failure.js
@@ -1,12 +1,15 @@
-import sad from 'src/assets/sad.png';
+import sad from '../assets/sad.png';
+import LogoMini from './LogoMini';
 
 export default function Failure(){
     return(
+        <>
+        <LogoMini/>
         <div className="box">
         <p className="font">Putz.. </p>
         <img src={sad} alt= "emoji chorando"/>
-        <p className="lettering">Você esqueceu alguns flashcards.. Não desanime! Na próxima você consegue!</p>
+        <p className = "lettering">Você esqueceu alguns flashcards..Não desanime! Na próxima você consegue!</p>
+        </div> 
+        </>
         );
-        </div>   
-    );
-}
+    }
