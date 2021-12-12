f04451f8a9e4e720d213f1647aef7e35ad477710
Isabela <isabela_cmoreira@hotmail.com>
2021-12-12 14:45:43 -0300

    Logo em miniatura componetizado

---
+++ src/components/LogoMini.js
@@ -0,0 +1,9 @@
+import miniatura from "../assets/miniatura.png";
+
+export default function LogoMini(){
+    return(
+        <>
+        <img className="logo-position" src={miniatura} alt="Logo em miniatura do jogo"/>
+        </>
+    );
+}
