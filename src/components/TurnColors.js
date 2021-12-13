b7f234fe75241d6b45dcec065eb7dc4d25abb768
Isabela <isabela_cmoreira@hotmail.com>
2021-12-12 21:53:48 -0300

    Componente que une adiciona borda ao flashcard

---
+++ src/components/TurnColors.js
@@ -0,0 +1,20 @@
+// export default function TurnBlack(){
+// return(
+
+// );
+// }
+// export default function TurnRed(){
+// return(
+
+// );
+// }
+// export default function TurnGreen(){
+// return(
+
+// );
+// }
+// export default function TurnYellow(){
+// return(
+
+// );
+// }
